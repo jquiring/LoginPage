c4ff4ff6e22f5b8dabaad7fabdfdc608d922817a
jquiring <jake.p.quiring@gmail.com>
2014-10-10 14:39:26 -0700

    pre-loaded code

--- LoginPage/ViewController.swift
+++ LoginPage/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//this is a comment in the view controller
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
